80a9c69279385bf03225314b788f52092cd2275c
Shaloom <shaloomniyibizi@gmail.com>
2024-02-20 08:34:02 +0200

    add second model

--- app.py
+++ app.py
@@ -1,44 +1,76 @@
+
+
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__,static_url_path='/static')
-model = pickle.load(open('model.pkl', 'rb'))
+model1 = pickle.load(open('model1.pkl', 'rb'))
+model2 = pickle.load(open('model2.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
+accuracy1 = pickle.load(open('accuracy1.pkl', 'rb'))
+accuracy2 = pickle.load(open('accuracy2.pkl', 'rb'))
 
-
+# Define a route for the home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Define a route for model prediction
 @app.route('/predict',methods=['POST'])
 def predict():
-
+    
+    # Get the input data from the form
     features = [float(x) for x in request.form.values()]
+    
+    # Convert the input data into numpy array
     final_features = [np.array(features)]
     final_features = scaler.transform(final_features)    
-    prediction = model.predict(final_features)
-    y_probabilities_test = model.predict_proba(final_features)
-    y_prob_success = y_probabilities_test[:, 1]
-    print("final features",final_features)
-    print("prediction:",prediction)
-    output = round(prediction[0], 2)
-    y_prob=round(y_prob_success[0], 3)
-    print(output)
+    
+    # Make prediction using the loaded model
+    predict1 = model1.predict(final_features)
+    predict2 = model2.predict(final_features)
+    y_prob1_test = model1.predict_proba(final_features)
+    y_prob2_test = model1.predict_proba(final_features)
+    y_prob1_success = y_prob1_test[:, 1]
+    y_prob2_success = y_prob2_test[:, 1]
+    
+    # print("final features",final_features)
+    # print("prediction 1:",predict1)
+    # print("prediction 2:",predict2)
+    
+    output1 = round(predict1[0], 2)
+    output2 = round(predict2[0], 2)
+    y_prob1=round(y_prob1_success[0], 3)
+    y_prob2=round(y_prob2_success[0], 3)
 
-    if output == 0:
-        return render_template('index.html', prediction_text='THE PATIENT IS MORE LIKELY TO HAVE A BENIGN CANCER WITH PROBABILITY VALUE  {}'.format(y_prob))
+    # print(output1)
+    # print(output2)
+    
+    if output1 == 0: 
+        ans1 = f'THE PATIENT IS MORE LIKELY TO HAVE A BENIGN CANCER WITH PROBABILITY VALUE  {y_prob1:.2f} AND ACCURACY OF {accuracy1:.2f}'
     else:
-         return render_template('index.html', prediction_text='THE PATIENT IS MORE LIKELY TO HAVE A MALIGNANT CANCER WITH PROBABILITY VALUE  {}'.format(y_prob))
+        ans1 = f'THE PATIENT IS MORE LIKELY TO HAVE A MALIGNANT CANCER WITH PROBABILITY VALUE  {y_prob1:.2f} AND ACCURACY OF {accuracy1:.2f}'
+    
+    if output2 == 0:
+        ans2 = f'THE PATIENT IS MORE LIKELY TO HAVE A BENIGN CANCER WITH PROBABILITY VALUE  {y_prob2:.2f}AND ACCURACY OF {accuracy2:.2f}'
+    else:
+        ans2 = f'THE PATIENT IS MORE LIKELY TO HAVE A MALIGNANT CANCER WITH PROBABILITY VALUE  {y_prob2:.2f} AND ACCURACY OF {accuracy1:.2f}'
+    
+    # Return the prediction result
+    return render_template('index.html', ans1 = ans1, accuracy1 = format(accuracy1  * 100,'.2f'), ans2 = ans2, accuracy2 = format(accuracy2  * 100,'.2f'), y_prob1 = format(y_prob1  * 100,'.2f'), y_prob2 = format(y_prob2  * 100,'.2f') )
+
         
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
 
     data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    predict1 = model1.predict([np.array(list(data.values()))])
+    predict2 = model2.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
+    output = predict1[0]
+    output = predict2[0]
     return jsonify(output)
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,16 +1,14 @@
 ### Import Libraries
 
+
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import warnings
 import pickle
-
+from sklearn.metrics import accuracy_score, classification_report
 warnings.filterwarnings('ignore')
 
 ### Import Datset
-df = pd.read_csv("data_breast-cancer-wiscons.csv")
+df = pd.read_csv("wiscons.csv")
 # we change the class values (at the column number 2) from B to 0 and from M to 1
 df.iloc[:,1].replace('B', 0,inplace=True)
 df.iloc[:,1].replace('M', 1,inplace=True)
@@ -34,22 +32,40 @@ x_train = scaler.fit_transform(X_train)
 x_test = scaler.transform(X_test)
 
 
-##
+# Train two different classification models
+# Train first using logistic regression classification models
 from sklearn.linear_model import LogisticRegression
-clf_lr = LogisticRegression()
-clf_lr.fit(x_train, y_train)
-predictions = clf_lr.predict(x_test)
+model1 = LogisticRegression()
+model1.fit(x_train, y_train)
+pred1 = model1.predict(x_test)
+accuracy1 = accuracy_score(y_test, pred1)
+
+# Train the second model using a RandomForestClassifier algorithm
+from sklearn.ensemble import RandomForestClassifier
+model2 = RandomForestClassifier(n_estimators=100, random_state=42)
+model2.fit(X_train, y_train)
+pred2 = model2.predict(x_test)
+accuracy2 = accuracy_score(y_test, pred2)
 
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
-print("Confusion Matrix : \n\n" , confusion_matrix(predictions,y_test))
+print("Confusion Matrix : \n\n" , confusion_matrix(pred1,y_test))
+print("Classification Report : \n\n" , classification_report(pred1,y_test),"\n")
 
-print("Classification Report : \n\n" , classification_report(predictions,y_test),"\n")
-
-
-pickle.dump(clf_lr, open('model.pkl', 'wb'))
 pickle.dump(scaler, open('scaler.pkl', 'wb'))
+scaler = pickle.load(open('scaler.pkl','rb'))
+print("scaller: ", scaler)
 
-model = pickle.load(open('model.pkl', 'rb'))
-print(model)
+pickle.dump(model1, open('model1.pkl', 'wb'))
+model1 = pickle.load(open('model1.pkl', 'rb'))
+print(model1)
+print("Model 1 Accuracy:", accuracy1)
+
+pickle.dump(model2, open('model2.pkl', 'wb'))
+model2 = pickle.load(open('model2.pkl', 'rb'))
+print(model2)
+print("Model 2 Accuracy:", accuracy2)
+
+pickle.dump(accuracy1, open('accuracy1.pkl','wb'))
+pickle.dump(accuracy2, open('accuracy2.pkl','wb'))
